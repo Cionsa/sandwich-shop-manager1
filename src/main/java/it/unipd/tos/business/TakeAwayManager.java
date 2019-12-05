f3a95e10a8f7d7c1bd710dd936c0585320b1347c
Matteo Lattanzio <daimatty@gmail.com>
2019-12-05 14:46:26 +0100

    close #4

--- src/main/java/it/unipd/tos/business/TakeAwayManager.java
+++ src/main/java/it/unipd/tos/business/TakeAwayManager.java
@@ -12,9 +12,21 @@ import it.unipd.tos.model.MenuItem;
 public class TakeAwayManager implements TakeAwayBill{
     public double getOrderPrice(List<MenuItem> itemsOrdered) throws TakeAwayBillException {
         double total = 0.0;
+        int nrPanini = 0;
+        double paninoLessExpensive = Double.MAX_VALUE;
 
         for (MenuItem menuItem : itemsOrdered) {
             total += menuItem.getPrice();
+            if(menuItem.getType() == MenuItem.items.Panino){
+                nrPanini++;
+                if(paninoLessExpensive > menuItem.getPrice()){
+                    paninoLessExpensive = menuItem.getPrice();
+                }
+            }
+        }
+
+        if(nrPanini > 5){
+            total = total - (paninoLessExpensive/2); 
         }
 
         return total;
--- src/test/java/it/unipd/tos/business/TakeAwayManagerTest.java
+++ src/test/java/it/unipd/tos/business/TakeAwayManagerTest.java
@@ -27,4 +27,29 @@ public class TakeAwayManagerTest{
             exc.getMessage();
         }
     }
+
+
+    @Test
+    public void HalfDiscountLessExpensivePaninoWith5PlusPaninoOrder_Test() throws TakeAwayBillException{
+        List<MenuItem> itemsOrdered = new ArrayList<MenuItem>();
+        TakeAwayManager testBill = new TakeAwayManager();
+
+        itemsOrdered.add(new MenuItem("Panino primavera", MenuItem.items.Panino, 4.00));
+        itemsOrdered.add(new MenuItem("Coca cola" , MenuItem.items.Bevanda, 1.50));
+        itemsOrdered.add(new MenuItem("Panino vegetariano", MenuItem.items.Panino, 3.50));
+        itemsOrdered.add(new MenuItem("Hot dog" , MenuItem.items.Panino, 4.00));
+        itemsOrdered.add(new MenuItem("Olive ascolane" , MenuItem.items.Fritto, 3.00));
+        itemsOrdered.add(new MenuItem("Panino primavera", MenuItem.items.Panino, 4.00));
+        itemsOrdered.add(new MenuItem("Coca cola" , MenuItem.items.Bevanda, 1.50));
+        itemsOrdered.add(new MenuItem("Panino vegetariano", MenuItem.items.Panino, 3.50));
+        itemsOrdered.add(new MenuItem("Hot dog" , MenuItem.items.Panino, 4.00));
+        itemsOrdered.add(new MenuItem("Olive ascolane" , MenuItem.items.Fritto, 3.00));
+
+        try {
+            assertEquals(30.25, testBill.getOrderPrice(itemsOrdered), 0.0);
+        } 
+        catch (TakeAwayBillException exc){
+            exc.getMessage();
+        }
+    }
 }
